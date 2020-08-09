846d4b4b7b1064c987a13d570ade6fe85f9d0a48
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2020-08-10 00:14:28 +0800

    removed old node_modules and added recipe files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
 class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+
+    // Trick the Access-Control-Allow-Origin
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://forkify-api.herokuapp.com/api/search?q=${recipeName}`);
+
+    const data = await api_call.json();
+    this.setState({recipes: data.recipes });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
+          <h1 className="App-title">Joyceeey's Kitchen Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={ this.state.recipes } />
       </div>
     );
   }
